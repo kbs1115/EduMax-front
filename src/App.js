b4b49952b089e17d612f2b85ef70c687747ea6b8
kbs1115 <bruce11158@gmail.com>
2024-04-07 02:56:50 +0900

    [feat] add pw change logic to modal

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/create-post" element={<PostCreatePage />} />
       </Routes>
-      <EmailModal />
+      <EmailModal isPassword={true}/>
     </>
   );
 }
--- src/components/modals/EmailModal.js
+++ src/components/modals/EmailModal.js
@@ -2,6 +2,203 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Typography from '../Typography';
 
+
+const EmailModal = ({isPassword = false}) => {
+	const [title, setTitle] = useState({
+		"title": "",
+		"step": ""
+	})
+	const [email, setEmail] = useState("");
+	const [pw1, setPw1] = useState("");
+	const [pw2, setPw2] = useState("");
+	const [certNum, setCertNum] = useState("");
+	const [isEmailValid, setIsEmailValid] = useState(false);
+	const [isPwValid, setIsPwValid] = useState(false);
+	const [step, setStep] = useState(1);
+
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
+
+	const handleCertNumChange = (event) => {
+		setCertNum(event.target.value);
+	};
+
+	useEffect(() => {
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w+)+$/;
+    setIsEmailValid(regex.test(email));
+		console.log(isEmailValid);
+  }, [email]);
+
+	useEffect(() => {
+    const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,20}$/;
+    setIsPwValid(regex.test(pw1));
+
+		if (pw1 !== pw2)
+			setIsPwValid(false);
+  }, [pw1, pw2]);
+
+	useEffect(() => {
+    if (step === 1) {
+			if (isPassword)
+				setTitle({
+					"title": "이메일 인증",
+					"step": "Step 1/3"
+				});
+			else
+				setTitle({
+					"title": "이메일 인증",
+					"step": "Step 1/2"
+				});
+		}
+		
+		if (step === 2) {
+			if (isPassword)
+				setTitle({
+					"title": "비밀번호 변경",
+					"step": "Step 2/3"
+				});
+			else
+				setTitle({
+					"title": "인증 완료",
+					"step": "Step 2/2"
+				});
+		}
+
+		if (step === 3)
+			setTitle({
+				"title": "변경 완료",
+				"step": "Step 3/3"
+			});
+  }, [step]);
+
+  return (
+    <Wrapper>
+			<div style={{ display: "flex", flexDirection: "column", width: "100%", paddingBottom: "10px" }}>
+				<TitleWrapper>
+					<Typography size='h2'>
+						{title["title"]}
+					</Typography>
+					<Typography size='h2' color='navy'>
+						{title["step"]}
+					</Typography>
+				</TitleWrapper>
+				{step === 1 && <>
+					<Typography size='body_content_thin' color='gray'>
+						본인확인을 위해 반드시 인증절차가 필요합니다.
+					</Typography>
+				</>}
+			</div>
+			{ step === 1 ? <>
+				<div style={{ display: "flex", width: "100%", flexDirection: "column", gap: "20px" }}>
+				<div style={{ display: "flex", justifyContent: "space-between", alignItems: "end"}}>
+					<EmailWrapper>
+						<Typography color='black_gray' size='h3_bold'>
+							이메일
+						</Typography>
+						<StyledInput 
+							value={email} 
+							onChange={handleEmailChange} 
+							placeholder="이메일을 입력하세요"
+						/>
+					</EmailWrapper>
+					<StyledButton disabled={!isEmailValid}>
+						<Typography 
+							size='body_sub_title' 
+							color={isEmailValid ? 'white' : 'gray'}>인증요청</Typography>
+					</StyledButton>
+				</div>
+				<CertNumWrapper>
+					<Typography color='black_gray' size='h3_bold'>
+						인증번호
+					</Typography>
+					<StyledInput 
+						value={certNum} 
+						onChange={handleCertNumChange} 
+						placeholder="인증번호를 입력하세요"
+					/>
+				</CertNumWrapper>
+			</div>
+			<div>
+				<StyledButton disabled={!(isEmailValid && certNum !== "")} onClick={() => setStep(2)}>
+					<Typography 
+						size='body_sub_title' 
+						color={isEmailValid ? 'white' : 'gray'}>다음</Typography>
+				</StyledButton>
+			</div>
+			</> : step === 2 && !isPassword ?
+			<Step2Wrapper>
+				<Step2TextWrapper>
+					<Typography color='gray' size='body_sub_title'>
+						귀하의 아이디는&nbsp; 
+					</Typography>
+					<Typography color='black_gray' size='body_sub_title'>
+						gmyun1999
+					</Typography>
+					<Typography color='gray' size='body_sub_title'>
+						입니다.
+					</Typography>
+				</Step2TextWrapper>
+				<div>
+					<StyledButton onClick={() => setStep(1)}>
+						<Typography 
+							size='body_sub_title' 
+							color='white'>홈으로</Typography>
+					</StyledButton>
+				</div>
+			</Step2Wrapper>
+			: step === 2 && isPassword ? <>
+			<div style={{ display: "flex", width: "100%", flexDirection: "column", gap: "20px" }}>
+			<CertNumWrapper>
+				<Typography color='black_gray' size='h3_bold'>
+					비밀번호
+				</Typography>
+				<StyledInput 
+					value={pw1}
+					type='password' 
+					onChange={(event) => setPw1(event.target.value)} 
+					placeholder="8~20자리/ 영문 대소문자, 숫자, 특수문자 조합"
+				/>
+			</CertNumWrapper>
+			<CertNumWrapper>
+				<Typography color='black_gray' size='h3_bold'>
+					비밀번호 확인
+				</Typography>
+				<StyledInput 
+					value={pw2} 
+					type='password' 
+					onChange={(event) => setPw2(event.target.value)} 
+					placeholder="8~20자리/ 영문 대소문자, 숫자, 특수문자 조합"
+				/>
+			</CertNumWrapper>
+		</div>
+		<div>
+			<StyledButton disabled={!isPwValid} onClick={() => setStep(3)}>
+				<Typography 
+					size='body_sub_title' 
+					color={isEmailValid ? 'white' : 'gray'}>다음</Typography>
+			</StyledButton>
+		</div>
+		</> : <Step2Wrapper>
+				<Step2TextWrapper>
+					<Typography color='gray' size='body_sub_title'>
+						귀하의 비밀번호가 성공적으로 변경되었습니다. 
+					</Typography>
+				</Step2TextWrapper>
+				<div>
+					<StyledButton onClick={() => setStep(1)}>
+						<Typography 
+							size='body_sub_title' 
+							color='white'>홈으로</Typography>
+					</StyledButton>
+				</div>
+			</Step2Wrapper>}
+    </Wrapper>
+  );
+}
+
+export default EmailModal;
+
 const Wrapper = styled.div`
   display: flex;
 	box-sizing: border-box;
@@ -89,104 +286,3 @@ const Step2TextWrapper = styled.div`
 	margin: auto;
 `;
 
-const EmailModal = () => {
-	const [email, setEmail] = useState("");
-	const [certNum, setCertNum] = useState("");
-	const [isEmailValid, setIsEmailValid] = useState(false);
-	const [step, setStep] = useState(1);
-
-	const handleEmailChange = (event) => {
-		setEmail(event.target.value);
-	};
-
-	const handleCertNumChange = (event) => {
-		setCertNum(event.target.value);
-	};
-
-	useEffect(() => {
-    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w+)+$/;
-    setIsEmailValid(regex.test(email));
-		console.log(isEmailValid);
-  }, [email]);
-
-  return (
-    <Wrapper>
-			<div style={{ display: "flex", flexDirection: "column", width: "100%", paddingBottom: "10px" }}>
-				<TitleWrapper>
-					<Typography size='h2'>
-						{step === 1 ? "이메일 인증" : "인증 완료"}
-					</Typography>
-					<Typography size='h2' color='navy'>
-						Step {step}/2
-					</Typography>
-				</TitleWrapper>
-				{step === 1 && <>
-					<Typography size='body_content_thin' color='gray'>
-						본인확인을 위해 반드시 인증절차가 필요합니다.
-					</Typography>
-				</>}
-			</div>
-			{ step === 1 ? <>
-				<div style={{ display: "flex", width: "100%", flexDirection: "column", gap: "20px" }}>
-				<div style={{ display: "flex", justifyContent: "space-between", alignItems: "end"}}>
-					<EmailWrapper>
-						<Typography color='black_gray' size='h3_bold'>
-							이메일
-						</Typography>
-						<StyledInput 
-							value={email} 
-							onChange={handleEmailChange} 
-							placeholder="이메일을 입력하세요"
-						/>
-					</EmailWrapper>
-					<StyledButton disabled={!isEmailValid}>
-						<Typography 
-							size='body_sub_title' 
-							color={isEmailValid ? 'white' : 'gray'}>인증요청</Typography>
-					</StyledButton>
-				</div>
-				<CertNumWrapper>
-					<Typography color='black_gray' size='h3_bold'>
-						인증번호
-					</Typography>
-					<StyledInput 
-						value={certNum} 
-						onChange={handleCertNumChange} 
-						placeholder="인증번호를 입력하세요"
-					/>
-				</CertNumWrapper>
-			</div>
-			<div>
-				<StyledButton disabled={!(isEmailValid && certNum !== "")} onClick={() => setStep(2)}>
-					<Typography 
-						size='body_sub_title' 
-						color={isEmailValid ? 'white' : 'gray'}>다음</Typography>
-				</StyledButton>
-			</div>
-			</>: 
-			<Step2Wrapper>
-				<Step2TextWrapper>
-					<Typography color='gray' size='body_sub_title'>
-						귀하의 아이디는&nbsp; 
-					</Typography>
-					<Typography color='black_gray' size='body_sub_title'>
-						gmyun1999
-					</Typography>
-					<Typography color='gray' size='body_sub_title'>
-						입니다.
-					</Typography>
-				</Step2TextWrapper>
-				<div>
-					<StyledButton onClick={() => setStep(1)}>
-						<Typography 
-							size='body_sub_title' 
-							color='white'>홈으로</Typography>
-					</StyledButton>
-				</div>
-			</Step2Wrapper>
-			}
-    </Wrapper>
-  );
-}
-
-export default EmailModal;
