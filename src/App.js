57eef1ce5ead0069f7a16540246ba7e037b15d88
gmyun1999 <dbsrbals26@gmail.com>
2024-05-02 23:46:05 +0900

    [add] web push

--- src/App.js
+++ src/App.js
@@ -16,12 +16,15 @@ import EmailModal from './components/modals/FindModal';
 import PostDetailPage from './pages/PostDetail';
 import MyPage from './pages/MyPage';
 
+
+
 function App() {
   const location = useLocation(); // 현재 경로를 얻기 위해 useLocation 훅 사용
 
   return (
     <>
-      {location.pathname !== '/login' && location.pathname !== '/signup' && location.pathname !== '/find' &&<NavBar />}
+
+      {location.pathname !== '/login' && location.pathname !== '/signup' && location.pathname !== '/find' && <NavBar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/question" element={<QuestionBoard />} />
@@ -34,7 +37,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/create-post" element={<PostCreatePage />} />
-        <Route path="/find" element={<EmailModal isPassword={true}/>} />
+        <Route path="/find" element={<EmailModal isPassword={true} />} />
         <Route path="/post/:postId" element={<PostDetailPage />} />
       </Routes>
     </>
---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB6Ik9RM45gQRmqiuC-PBZcNjumLQ0I94A",
+  authDomain: "edumax-414706.firebaseapp.com",
+  projectId: "edumax-414706",
+  messagingSenderId: "356957646824",
+  appId: "1:356957646824:web:8b3defe369923f51067e5d"
+};
+
+export default firebaseConfig;
